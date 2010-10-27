56bc167a8935b292cdf50bcbf4d6e7919980a3cd
TANABE Ken-ichi <nabeken@tknetworks.org>
2010-10-27 09:41:02 +0900

    Add correct license

--- bacula/metadata.rb
+++ bacula/metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "Example Com"
-maintainer_email "ops@example.com"
+maintainer       "TANABE Ken-ichi"
+maintainer_email "nabeken@tknetworks.org"
 license          "Apache 2.0"
 description      "Installs/Configures bacula"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
--- debian/metadata.rb
+++ debian/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "TKNetworks"
 maintainer_email "nabeken@tknetworks.org"
-license          "All rights reserved"
+license          "Apache 2.0"
 description      "Installs/Configures debian"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
