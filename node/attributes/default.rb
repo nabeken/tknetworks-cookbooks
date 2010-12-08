b41c35e233b0c4c86222f45399d8582700a606db
TANABE Ken-ichi <nabeken@tknetworks.org>
2010-12-08 23:17:48 +0900

    node: catch correct exceptions

--- node/attributes/default.rb
+++ node/attributes/default.rb
@@ -1,5 +1,5 @@
 begin
   include_attribute "node::#{hostname}"
-rescue
-  Chef::Log.inf("failed to load attribute node::#{hostname}")
+rescue Chef::Exceptions::AttributeNotFound
+  Chef::Log.debug("failed to load attribute node::#{hostname}")
 end
--- node/recipes/default.rb
+++ node/recipes/default.rb
@@ -1,5 +1,1 @@
-begin
-  include_recipe "node::#{node.hostname}"
-rescue
-  Chef::Log.info("failed to load node::#{node.hostname}")
-end
+include_recipe "node::#{node.hostname}"
