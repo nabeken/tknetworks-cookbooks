9bc953e9e7c2335c64d275ef45788f1b0c2f2b20
TANABE Ken-ichi <nabeken@tknetworks.org>
2010-12-21 00:24:04 +0900

    freebsd: use rsync.jp.g.org

--- freebsd/recipes/portage.rb
+++ freebsd/recipes/portage.rb
@@ -3,7 +3,7 @@ package "net/rsync" do
 end
 
 cron "portage_sync" do
-  command "/usr/local/bin/rsync rsync://rsync.gentoo.org/gentoo-portage /srv/portage -aqP --delete"
+  command "/usr/local/bin/rsync rsync://rsync.jp.gentoo.org/gentoo-portage /srv/portage -aqP --delete"
   hour "*/4"
   minute 0
 end
