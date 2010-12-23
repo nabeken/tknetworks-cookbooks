c9a516c631ee0a7fd965536acdf7d0d090647eff
TANABE Ken-ichi <nabeken@tknetworks.org>
2010-12-23 17:35:05 +0900

    rsync: restart if stopped

--- rsync/recipes/server.rb
+++ rsync/recipes/server.rb
@@ -1,13 +1,10 @@
 service node.rsync.server.service do
   action :enable
+  running true
 end
 
 begin
   include_recipe "rsync::#{node.hostname}"
 rescue ArgumentError
   Chef::Log.info("failed to load rsync::#{node.hostname}")
-end
-
-service node.rsync.server.service do
-  action :restart
 end
