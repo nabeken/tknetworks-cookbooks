4162eab591ebc09c7d91794f25c2d81e0ed61819
TANABE Ken-ichi <nabeken@tknetworks.org>
2011-02-20 21:23:02 +0900

    xen: Fix root

--- xen/attributes/default.rb
+++ xen/attributes/default.rb
@@ -1,6 +1,6 @@
 default[:xen][:domU][:kernel]  = "/boot/xen/kernel-genkernel-x86_64-2.6.37-gentoo-domU"
 default[:xen][:domU][:ramdisk] = "/boot/xen/initramfs-genkernel-x86_64-2.6.37-gentoo-domU"
-default[:xen][:domU][:root]    = "/dev/ram0 ro console=hvc0 real_root=/dev/xvda2"
+default[:xen][:domU][:root]    = "/dev/ram0 ro console=hvc0 real_root=/dev/xvda1"
 default[:xen][:domU][:extra]   = "xencons=tty"
 default[:xen][:domU][:cpus]    = [0, 1]
 default[:xen][:domU][:vcpus]   = 2
