bc9236349f1241f313d1f444b7a16597439c6285
TANABE Ken-ichi <nabeken@tknetworks.org>
2011-02-25 20:32:28 +0900

    bacula: restructuring bacula-dir.conf

--- bacula/recipes/dir.rb
+++ bacula/recipes/dir.rb
@@ -15,20 +15,7 @@ service node.bacula.dir.service do
     action :enable
 end
 
-directory "#{node.bacula.dir.config_dir}/bacula-dir.conf.d" do
-    owner  node.bacula.uid
-    group  node.bacula.gid
-    mode   0644
-end
-
-execute "create_bacula_dir_conf" do
-    command "cat #{node.bacula.dir.config_dir}/bacula-dir.conf.d/* > #{node.bacula.dir.config_dir}/bacula-dir.conf"
-    action :nothing
-    notifies :restart, resources(:service => node.bacula.dir.service)
-end
-
-%w{
-    dir.conf
+dir_confs = %w{
     catalog.conf
     console.conf
     message.conf
@@ -36,7 +23,9 @@ end
     pool.conf
     schedule.conf
     jobdefs.conf
-}.each do |f|
+}
+
+dir_confs.each do |f|
     template "#{node.bacula.dir.config_dir}/bacula-dir.conf.d/#{f}" do
         source "bacula-dir/#{f}"
         owner  node.bacula.uid
@@ -49,11 +38,30 @@ end
             :pid_dir      => node.bacula.pid_dir,
             :conf_dir     => node.bacula.config_dir,
             :query_file   => node.bacula.dir.query_file,
-            :console_password  => node.bacula.dir.console_password,
+            :console_password        => node.bacula.dir.console_password,
             :maximum_concurrent_jobs => node.bacula.maximum_concurrent_jobs
         })
-        notifies :run, resources(:execute => "create_bacula_dir_conf")
+        notifies :restart, resources(:service => node.bacula.dir.service)
     end
+end
+
+template "#{node.bacula.dir.config_dir}/bacula-dir.conf" do
+    source "bacula-dir.conf"
+    owner  node.bacula.uid
+    group  node.bacula.gid
+    mode   0644
+    variables({
+        :dir_confs    => dir_confs
+        :dir_hostname => node.hostname,
+        :tls          => node.bacula.tls,
+        :working_dir  => node.bacula.working_dir,
+        :pid_dir      => node.bacula.pid_dir,
+        :conf_dir     => node.bacula.config_dir,
+        :query_file   => node.bacula.dir.query_file,
+        :console_password        => node.bacula.dir.console_password,
+        :maximum_concurrent_jobs => node.bacula.maximum_concurrent_jobs
+    })
+    notifies :restart, resources(:service => node.bacula.dir.service)
 end
 
 search(:node, "role:bacula_fd") do |n|
