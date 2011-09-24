39e83e361db68f9693a8ad1d12bb514d5358de92
TANABE Ken-ichi <nabeken@tknetworks.org>
2011-09-25 01:17:36 +0900

    apache2: version 0.0.2

--- apache2/metadata.rb
+++ apache2/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "TKNetworks"
 maintainer_email "nabeken@tknetworks.org"
-license          ""
+license          "Apache 2.0"
 description      "Installs/Configures apache2"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
