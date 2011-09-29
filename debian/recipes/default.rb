ff5713684ffe273aeb22d7b9b9816a70a34a032e
TANABE Ken-ichi <nabeken@tknetworks.org>
2011-09-30 00:17:15 +0900

    debian: indent cleanup

--- debian/recipes/default.rb
+++ debian/recipes/default.rb
@@ -5,26 +5,29 @@ execute "apt-get update" do
 end
 
 debian_aptline "security" do
-    url  "http://security.debian.org/"
-    path "/updates"
-    repo %w{main contrib}
+  url  "http://security.debian.org/"
+  path "/updates"
+  repo %w{main contrib}
+  notifies :run, "execute[apt-get update]", :immediately
 end
 
 debian_aptline "base" do
-    url  "http://cdn.debian.net/debian"
-    repo %w{main contrib nonfree}
+  url  "http://cdn.debian.net/debian"
+  repo %w{main contrib nonfree}
+  notifies :run, "execute[apt-get update]", :immediately
 end
 
 # opscode's cool repos
 debian_aptline "opscode" do
-    url  "http://apt.opscode.com"
-    repo %w{main}
-    release "#{node.debian.release}-0.10"
+  url  "http://apt.opscode.com"
+  repo %w{main}
+  release "#{node.debian.release}-0.10"
+  notifies :run, "execute[apt-get update]", :immediately
 end
 
 # remove execute bits
 file "/etc/cron.daily/find" do
-    mode 0444
+  mode 0444
 end
 
 %w{
@@ -32,13 +35,13 @@ end
   rsync
 }.each do |pkg|
   package pkg do
-      action :install
+    action :install
   end
 end
 
 cookbook_file "/etc/cron-apt/config" do
-    source "etc/cron-apt/config"
-    owner "root"
-    group "root"
-    mode  0644
+  source "etc/cron-apt/config"
+  owner "root"
+  group "root"
+  mode  0644
 end
