2c208b1e05b844d7be9dcb8211a307df08b92355
TANABE Ken-ichi <nabeken@tknetworks.org>
2011-10-01 05:48:26 +0900

    debian: Fix resource name

--- debian/recipes/default.rb
+++ debian/recipes/default.rb
@@ -1,20 +1,21 @@
 return if node.platform != "debian"
 
-execute "apt-get update" do
+execute "apt-get-update" do
   command "/usr/bin/apt-get -q update"
+  action :nothing
 end
 
 debian_aptline "security" do
   url  "http://security.debian.org/"
   path "/updates"
   repo %w{main contrib}
-  notifies :run, "execute[apt-get update]", :immediately
+  notifies :run, "execute[apt-get-update]", :immediately
 end
 
 debian_aptline "base" do
   url  "http://cdn.debian.net/debian"
-  repo %w{main contrib nonfree}
-  notifies :run, "execute[apt-get update]", :immediately
+  repo %w{main contrib non-free}
+  notifies :run, "execute[apt-get-update]", :immediately
 end
 
 # opscode's cool repos
@@ -22,7 +23,7 @@ debian_aptline "opscode" do
   url  "http://apt.opscode.com"
   repo %w{main}
   release "#{node.debian.release}-0.10"
-  notifies :run, "execute[apt-get update]", :immediately
+  notifies :run, "execute[apt-get-update]", :immediately
 end
 
 # remove execute bits
