bc05a2972a889ddb5a8ef4358b42687e30113f55
TANABE Ken-ichi <nabeken@tknetworks.org>
2012-01-10 01:27:34 +0900

    freebsd: Do not install cron[portsnap] in jail guest, Add shells/bash

--- freebsd/recipes/default.rb
+++ freebsd/recipes/default.rb
@@ -9,4 +9,11 @@ cron "portsnap" do
   command "/usr/sbin/portsnap cron update"
   hour 3
   minute 0
+  not_if do
+    node[:virtualization][:system] == "jail" and node[:virtualization][:role] == "guest"
+  end
+end
+
+package "shells/bash" do
+  source "ports"
 end
