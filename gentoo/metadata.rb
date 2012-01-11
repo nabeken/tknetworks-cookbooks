c4eba54a3d73a7072417fe82a4219337cfb907a7
TANABE Ken-ichi <nabeken@tknetworks.org>
2012-01-11 19:58:58 +0900

    gentoo: Fix indent, Add portage cookbook as deps

--- gentoo/metadata.rb
+++ gentoo/metadata.rb
@@ -4,3 +4,4 @@ license          ""
 description      "Installs/Configures gentoo"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+depends          "portage"
--- gentoo/recipes/default.rb
+++ gentoo/recipes/default.rb
@@ -11,7 +11,7 @@ extend Chef::Gentoo
 arch = getKeywords(node)
 
 link "/etc/make.profile" do
-    to "/usr/portage/profiles/default/linux/#{arch}/#{node.gentoo.release}/#{node.gentoo.profile}"
+  to "/usr/portage/profiles/default/linux/#{arch}/#{node.gentoo.release}/#{node.gentoo.profile}"
 end
 
 %w{
@@ -20,13 +20,13 @@ end
   app-portage/gentoolkit
 }.each do |pkg|
   package pkg do
-      action :install
+    action :install
   end
 end
 
 portage_use "app-portage/layman" do
-    enable %w(cvs git subversion)
-    notifies :reinstall, resources(:package => "app-portage/layman")
+  enable %w(cvs git subversion)
+  notifies :reinstall, resources(:package => "app-portage/layman")
 end
 
 # useful USE flags
@@ -42,49 +42,47 @@ portage_use "net-misc/ntp" do
 end
 
 %w{
-    /etc/make.conf.local
-    /var/lib/layman/make.conf
+  /etc/make.conf.local
+  /var/lib/layman/make.conf
 }.each do |f|
-    unless File.exist?(f)
-        Chef::Log.info("touching #{f}")
-        FileUtils.mkdir_p(File.dirname(f))
-        File.open(f) do |f|
-          f.puts "PORTDIR_OVERLAY="
-          f.puts "$PORTDIR_OVERLAY"
-          f.puts "\""
-        end
+  unless File.exist?(f)
+    Chef::Log.info("touching #{f}")
+    FileUtils.mkdir_p(File.dirname(f))
+    File.open(f, "w+") do |f|
+      f.puts 'PORTDIR_OVERLAY="$PORTDIR_OVERLAY"'
     end
+  end
 end
 
 config = {
-    :use        => node.gentoo.default_use,
-    :keywords   => getKeywords(node),
-    :chost      => getChost(node),
-    :cflags     => getCflags(node),
-    :makeopts   => getMakeopts(node),
-    :portdir    => "/usr/portage",
-    :features   => node.gentoo.features,
-    :mirrors    => getMirrors(node),
-    :sync       => getSync(node),
-    :rsync_opts => node.gentoo.rsync_opts,
-    :linguas    => node.gentoo.linguas,
-    :license    => node.gentoo.license,
-    :apache2_modules => node.gentoo.apache2_modules,
-    :portdir_overlay => "/usr/local/portage"
+  :use        => node.gentoo.default_use,
+  :keywords   => getKeywords(node),
+  :chost      => getChost(node),
+  :cflags     => getCflags(node),
+  :makeopts   => getMakeopts(node),
+  :portdir    => "/usr/portage",
+  :features   => node.gentoo.features,
+  :mirrors    => getMirrors(node),
+  :sync       => getSync(node),
+  :rsync_opts => node.gentoo.rsync_opts,
+  :linguas    => node.gentoo.linguas,
+  :license    => node.gentoo.license,
+  :apache2_modules => node.gentoo.apache2_modules,
+  :portdir_overlay => "/usr/local/portage"
 }
 
 template "/etc/make.conf" do
-    source "etc/make.conf"
-    owner "root"
-    group "root"
-    mode  0644
-    variables :config => config
+  source "etc/make.conf"
+  owner "root"
+  group "root"
+  mode  0644
+  variables :config => config
 end
 
 template "/etc/make.conf.use" do
-    source "etc/make.conf.use"
-    owner "root"
-    group "root"
-    mode  0644
-    variables :use => node.gentoo.use
+  source "etc/make.conf.use"
+  owner "root"
+  group "root"
+  mode  0644
+  variables :use => node.gentoo.use
 end
