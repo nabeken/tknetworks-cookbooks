d04cbf6b4cea92c9f0eec713e2293712c1dcacf7
TANABE Ken-ichi <nabeken@tknetworks.org>
2012-05-01 23:19:44 +0900

    openvpn: Add openvpn_interface definition to generate /etc/hostname.if

--- openvpn/definitions/default.rb
+++ openvpn/definitions/default.rb
@@ -62,3 +62,46 @@ define :openvpn_server,
         end
   end
 end
+
+define :openvpn_interface,
+       :inet  => nil,
+       :inet6 => nil,
+       :dev   => nil,
+       :extra_commands => nil do
+
+  if node[:platform] != "openbsd"
+    raise "openvpn_interface is only for OpenBSD"
+  end
+
+  if params[:inet].nil? && params[:inet6].nil?
+    raise "ipv4 or ipv6 address required"
+  end
+
+  if params[:dev].nil?
+    raise "dev is required"
+  end
+
+  if params[:extra_commands].nil?
+    extra_commands = []
+  else
+    extra_commands = params[:extra_commands]
+  end
+
+  begin
+    t = resources("template[/etc/hostname.#{params[:dev]}")
+  rescue
+    t = template "/etc/hostname.#{params[:dev]}" do
+          owner "root"
+          group node[:etc][:passwd][:root][:gid]
+          mode  0640
+          cookbook "openvpn"
+          variables({
+            :config => "#{node[:openvpn][:dir]}/#{params[:name]}.conf",
+            :inet   => params[:inet],
+            :inet6  => params[:inet6],
+            :extra_commands => extra_commands
+          })
+          source "hostname.tun"
+        end
+  end
+end
