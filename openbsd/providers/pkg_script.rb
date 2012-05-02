3766528993e8a96b6512af9bb899fc9d8273a41a
TANABE Ken-ichi <nabeken@tknetworks.org>
2012-05-02 23:07:14 +0900

    openbsd: initial import

---
+++ openbsd/providers/pkg_script.rb
@@ -0,0 +1,73 @@
+#
+# Author:: Ken-ichi TANABE (<nabeken@tknetworks.org>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'chef/mixin/shell_out'
+include Chef::Mixin::ShellOut
+
+action :enable do
+  if @is_absent
+    # Add
+    @pkgs.push(@new_resource.name)
+    @pkgs.uniq!
+    # write
+    write_pkg_scripts
+    @new_resource.updated_by_last_action(true)
+  else
+    Chef::Log.info("#{@new_resource.name} is already enabled")
+  end
+end
+
+action :disable do
+  if !@is_absent
+    # remove
+    @pkgs.delete(@new_resource.name)
+    @pkgs.uniq!
+    # write
+    write_pkg_scripts
+    @new_resource.updated_by_last_action(true)
+  end
+end
+
+action :start do
+  result = shell_out!("/etc/rc.d/#{@new_resource.name} start", :env => nil)
+  result.error!
+end
+
+def load_current_resource
+  raise "only for OpenBSD" if node[:platform] != "openbsd"
+  @current_resource = Chef::Resource::OpenbsdPkgScript.new(@new_resource.name)
+
+  if ::File.exists?(@new_resource.pkg_scripts_path)
+    @pkgs = ::File.open(@new_resource.pkg_scripts_path).read.strip.split(/[\s\n]/)
+  else
+    @pkgs = []
+  end
+  @is_absent = !@pkgs.include?(@new_resource.name)
+  @current_resource
+end
+
+protected
+
+def write_pkg_scripts
+  if !@pkgs.empty?
+    ::File.open(@new_resource.pkg_scripts_path, "w") { |f|
+      f.write @pkgs.join("\n") + "\n"
+    }
+    link "/etc/rc.d/#{@new_resource.name}" do
+      to "/etc/rc.d/openvpn"
+    end
+  end
+end
---
+++ openbsd/recipes/default.rb
@@ -0,0 +1,36 @@
+#
+# Author:: Ken-ichi TANABE (<nabeken@tknetworks.org>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+return if node[:platform] != "openbsd"
+
+execute "openbsd-add-manip-pkg-script" do
+  oneliner = "[ -f /etc/pkg_scripts ] && for _r in `cat #{node[:openbsd][:pkg_scripts]}`; do pkg_scripts=\"${pkg_scripts} ${_r}\"; done"
+  rc = "/etc/rc.conf.local"
+  command "echo >> #{rc}; echo \"# DO NOT APPEND ANY LINES BELOW\" >> #{rc}; echo '#{oneliner}' >> #{rc}"
+  not_if do
+    ::File.open("/etc/rc.conf.local").readlines.any? { |l|
+      l.start_with?(oneliner)
+    }
+  end
+end
+
+openbsd_pkg_script "openvpn_node_gw_client" do
+  action :enable
+end
+
+openbsd_pkg_script "openvpn_rt_gw_client" do
+  action :enable
+end
